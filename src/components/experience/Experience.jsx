c174d3cfed06d7db38440ef48286ad96ceab9d4e
ChinepohDivine-f <divinechinepoh@gmail.com>
2023-05-27 19:28:12 +0100

    services

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,69 +1,16 @@
 import React from "react";
 import "./experience.css";
-import {BsPatchCheckFill} from 'react-icons/bs'
+import ListExperience from "./components/listExperience";
 
-const Expenrience = () => {
+export const Experience = () => {
   return (
     <section id="experience">
-      <h5>What Skills I Have </h5>
+      <h5>What skills i have</h5>
       <h2>My Experience</h2>
-
       <div className="container experience__container">
-        <div div className="experience__frontend">
-          <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light"></small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>HTML</h4>
-              <small className="text-light"></small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>CSS</h4>
-              <small className="text-light"></small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>JAVA</h4>
-              <small className="text-light"></small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>REACT</h4>
-              <small className="text-light"></small>
-            </article>
-          </div>
-          END OF FRONT END
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>PYTHON</h4>
-              <small className="text-light"></small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>MONGO</h4>
-              <small className="text-light"></small>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill />
-              <h4>JAVASCRIPT</h4>
-              <small className="text-light"></small>
-            </article>
-
-    
-          </div>
-          END OF BACKEND
-        </div>
-        <div className="experience__backend"></div>
+        <ListExperience />
       </div>
     </section>
   );
 };
-
-export default Expenrience;
+export default Experience;
